41097092de980c9244dbd1a9d9600e1c3398da38
yenyang <tjkelley19@gmail.com>
2023-12-30 11:27:54 -0800

    Patched toolbar ui system on update patch to set m_UniqueAssetStatusChanged to false.

---
+++ Anarchy-BepInEx/Patches/ToolbarUISystemOnUpdatePatch.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ToolbarUISystemOnUpdatePatch.cs" company="Yenyang's Mods. MIT License">
+// Copyright (c) Yenyang's Mods. MIT License. All rights reserved.
+// </copyright>
+
+namespace Anarchy.Patches
+{
+    using Anarchy.Utils;
+    using Game.UI.InGame;
+    using HarmonyLib;
+    using Unity.Entities;
+
+    /// <summary>
+    /// Patches Unique Asset Tracking System IsPlacedUniqueAsset to return false.
+    /// </summary>
+    [HarmonyPatch(typeof(ToolbarUISystem), "OnUpdate")]
+    public class ToolbarUISystemOnUpdatePatch
+    {
+        /// <summary>
+        /// Patches Unique Asset Tracking System IsPlacedUniqueAsset to return false.
+        /// </summary>
+        public static void Prefix()
+        {
+            ToolbarUISystem toolbarUISystem = World.DefaultGameObjectInjectionWorld?.GetOrCreateSystemManaged<ToolbarUISystem>();
+            toolbarUISystem.SetMemberValue("m_UniqueAssetStatusChanged", false);
+        }
+    }
+}
